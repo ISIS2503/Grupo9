8fafc239eb00c44b80813ffb4a21e3f9b88787f0
n.acevedos <n.acevedos@ISIS2503-0209.sis.virtual.uniandes.edu.co>
2017-11-11 11:25:38 -0500

    Tests

--- JPANoSQLConcourseApp/src/main/java/co/edu/uniandes/isis2503/nosqljpa/service/COResource.java
+++ JPANoSQLConcourseApp/src/main/java/co/edu/uniandes/isis2503/nosqljpa/service/COResource.java
@@ -24,6 +24,7 @@ import javax.ws.rs.core.MediaType;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @Path("/co")
+@Secured({Role.Admin})
 public class COResource {
     
     private COLogic coLogic;  
