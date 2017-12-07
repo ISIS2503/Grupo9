569cce7b40c9076821d618c9b7e533bfa397f9a6
ne.cabrera <ne.cabrera@ISIS2503-0187.sis.virtual.uniandes.edu.co>
2017-12-06 20:07:41 -0500

    Paginacion casi lista

--- Front-with-auth0/04-Authorization/src/Home/Home.js
+++ Front-with-auth0/04-Authorization/src/Home/Home.js
@@ -8,71 +8,70 @@ import RTChart from 'react-rt-chart';
 const DOMNode = document.getElementById('renderTarget');
 
 class Home extends Component {
-constructor(props){
-    super(props);
-    
-     this.state={
-        value: 0,
-        time: new Date()
-    };
-    
-    this.handle = this.handle.bind(this);
+    constructor(props) {
+        super(props);
 
-    this.socket = io.connect("http://localhost:8086/thermalcomfort");
-    this.socket.on('mesurements', msg => {
-         console.log(msg);
-         var mensaje = msg.replace("'","\"");
-        var json = JSON.parse(mensaje);
-        console.log(json.value);
+        this.state = {
+            value: 0,
+            time: new Date()
+        };
+
+        this.handle = this.handle.bind(this);
+
+        this.socket = io.connect("http://localhost:8086/thermalcomfort");
+        this.socket.on('mesurements', msg => {
+            console.log(msg);
+            var mensaje = msg.replace("'", "\"");
+            var json = JSON.parse(mensaje);
+            console.log(json.value);
+            this.setState({
+                value: JSON.parse(json.value),
+                time: JSON.parse(json.time)
+
+            });
+        });
+
+
+    }
+
+    handle(msg) {
         this.setState({
-        value: JSON.parse(json.value),
-        time: JSON.parse(json.time)
-        
-    });
-    });
+            value: msg.value,
+            time: msg.time
 
-    
-}
-
-handle(msg){
-    this.setState({
-        value: msg.value,
-        time: msg.time
-        
-    });
-    console.log(msg.value);
-}
+        });
+        console.log(msg.value);
+    }
 
     render() {
         const {isAuthenticated, login} = this.props.auth;
         var data = {
-                  date: new Date(),
-                  Data: this.state.value
-                };
+            date: new Date(),
+            Data: this.state.value
+        };
         return (
                 <div className="container">
                     {this.state.value}
                     {
                         isAuthenticated() && (
-                            
-                            <RTChart
-                            fields={['Data']}
-                            data={data} />
-                            
-                        )
+                                        <RTChart
+                                            fields={['Data']}
+                                            data={data} />
+
+                                    )
                     }
                     {
-                        !isAuthenticated() && (
-                                    <h4>
-                                        You are not logged in! Please{' '}
-                                        <a style={{cursor: 'pointer'}}
-                                           onClick={login.bind(this)}
-                                           >
-                                            Log In
-                                        </a>
-                                        {' '}to continue.
-                                    </h4>
-                                            )
+                    !isAuthenticated() && (
+                                        <h4>
+                                            You are not logged in! Please{' '}
+                                            <a style={{cursor: 'pointer'}}
+                                               onClick={login.bind(this)}
+                                               >
+                                                Log In
+                                            </a>
+                                            {' '}to continue.
+                                        </h4>
+                                        )
                     }
                 </div>
                             );
