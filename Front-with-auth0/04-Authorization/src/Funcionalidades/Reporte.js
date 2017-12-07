a033a69c9818e60e827c2122182bbcb3c3e452ce
jc.sanchez16 <jc.sanchez16@ISIS2503-0229.sis.virtual.uniandes.edu.co>
2017-12-07 14:23:37 -0500

    pag

--- Front-with-auth0/04-Authorization/src/Funcionalidades/Reporte.js
+++ Front-with-auth0/04-Authorization/src/Funcionalidades/Reporte.js
@@ -8,15 +8,55 @@ import React from 'react';
 import { Button } from 'reactstrap';
 
 class Reporte extends React.Component {
+    getblah() {
+        axios.get('http://localhost:8084/reporte')
+                .then(response => this.setState({reporte: response.data.medidas}))
+                .catch(error => this.setState({message: error.message}));
+    }
   render() {
-    return (
-        <div>    
+        return (
+                <div>  
+           <Breadcrumb>
+        <BreadcrumbItem><a href="por">Funcionalidades</a></BreadcrumbItem>
+        <BreadcrumbItem active>Reporte</BreadcrumbItem>
+      </Breadcrumb>
         <Button color="primary">Reporte</Button>{' '}
-        </div>
-        
-      
-    );
-  }
+        <table>
+            <tr>
+              <th>Reporte</th>
+            </tr>
+            <tr>
+              <td>Numero de Alertas Fuera De Rango</td>
+              <td>reporte.AFR</td>
+            </tr>
+            <tr>
+              <td>Numero de Alertas Fuera de Linea</td>
+              <td>reporte.AFL</td>
+            </tr>
+            <tr>
+              <td>Promedio Ruido</td>
+              <td>reporte.Ruido</td>
+            </tr>
+            <tr>
+              <td>Promedio Temperatura</td>
+              <td>reporte.Temperatura</td>
+            </tr>
+            <tr>
+              <td>Promedio Iluminacion</td>
+              <td>reporte.Luz</td>
+            </tr>
+            <tr>
+              <td>Promedio Gases</td>
+              <td>reporte.CO</td>
+            </tr>
+          </table>
+             
+         
+                </div>
+
+
+                );
+    }
 }
 export default Reporte;
 
